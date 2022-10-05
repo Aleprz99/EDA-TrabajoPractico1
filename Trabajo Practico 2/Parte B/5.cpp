be8a1443ba85923e3e61cd9fe49d48e7c0465f51
Aleprz99 <112905506+Aleprz99@users.noreply.github.com>
2022-10-05 02:10:38 -0300

    Add files via upload

---
+++ Trabajo Practico 2/Parte B/5.cpp
@@ -0,0 +1,24 @@
+#include "iostream"
+#include "stdio.h"
+#include "conio.h"
+
+int main(){
+	int vsize,min;
+	printf("Ingrese el largo del vector\n");
+	scanf("%d",&vsize);
+	int vector[vsize];
+	printf("--------------------------\nIngrese los valores del vector:\n");
+	for(int i=0;i<vsize;i++){
+		scanf("%d",&vector[i]);
+		if(i==0){
+			min=vector[i];
+		}else{
+			if(min>vector[i]){
+				min=vector[i];
+			}
+		}
+	}
+	printf("--------------------------\nEl menor numero del arreglo es: %d\nPresione cualquier tecla para salir",min);
+	getch();
+	return 0;
+}
